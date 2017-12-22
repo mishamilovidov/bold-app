c88c46b338169909fa987e985b0cfc0b2ca92298
Misha Milovidov <m.milovidov@icloud.com>
2017-12-22 04:52:03 +0000

    Connect to sqlite db instead of RDS

--- boldapp/settings.py
+++ boldapp/settings.py
@@ -88,15 +88,10 @@ STATIC_URL = '/static/'
 
 DATABASES = {
     'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
